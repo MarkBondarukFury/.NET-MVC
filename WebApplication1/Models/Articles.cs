ea5f1f3da6b7b33c592184cfdf647ebf14c90c43
MarkBondarukFury <59942592+MarkBondarukFury@users.noreply.github.com>
2020-02-11 23:12:11 +0200

    Initial commit

---
+++ WebApplication1/Models/Articles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Articles
+    {
+        public IList<Article> ArticleContainer;
+
+        public Articles()
+        {
+            ArticleContainer = new List<Article>();
+
+            for (int i = 1; i <= 30; i++)
+                ArticleContainer.Add(new Article($"Article {i}", $"Some information about something {i}", DateTime.Now));
+        }
+    }
+}
---
+++ WebApplication1/Models/ListHelpers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Models
+{
+    public static class ListHelpers
+    {
+        public static MvcHtmlString CreateUnorderedList(this HtmlHelper html, string[] items, string name)
+        {
+            var text = new TagBuilder("p");
+            text.SetInnerText(name);
+            var list = new TagBuilder("ul");
+            foreach (string item in items)
+            {
+                var elem = new TagBuilder("li");
+                elem.SetInnerText(item);
+                list.InnerHtml += elem.ToString(); 
+            }
+            text.InnerHtml += list.ToString();
+            return new MvcHtmlString(text.ToString());
+        }
+        public static MvcHtmlString CreateOrderedList(this HtmlHelper html, string[] items, string name)
+        {
+            var text = new TagBuilder("p");
+            text.SetInnerText(name);
+            var list = new TagBuilder("ol");
+            foreach (string item in items)
+            {
+                var elem = new TagBuilder("li");
+                elem.SetInnerText(item);
+                list.InnerHtml += elem.ToString();
+            }
+            text.InnerHtml += list.ToString();
+            return new MvcHtmlString(text.ToString());
+        }
+    }
+}
